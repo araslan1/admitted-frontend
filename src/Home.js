8dff8429cab4d496696cb2c3146acd6d7d7a164f
Adam Raslan <araslan@usc.edu>
2023-06-13 15:51:00 -0500

    Commit

--- src/Home.js
+++ src/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
             <h1>Turning Essays into Acceptances</h1>
             </div>
             <div>
-            <p>Win your admissions officers over with the #1 platform to have your essays reviewed by current students who have already been admitted!</p>
+            <p>Win over college admission officers with the #1 essay review platform, where your essays are edited by students who have already been admitted into top unversities!</p>
             </div>
             <Link to='/signup'>Get Started!</Link>
         </div>
